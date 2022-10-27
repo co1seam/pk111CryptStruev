8d73e0cc22b7e9e9a02e3c42ec58bff4f7aed2e8
Constantine Struev <guystruev@gmail.com>
2022-10-27 15:13:01 +0300

    main

--- Program.cs
+++ Program.cs
@@ -10,12 +10,16 @@ namespace pk111CryptStruev
     {
         static void Main(string[] args)
         {
+            string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            Console.WriteLine(alphabet[3]);
             Console.WriteLine("Добро пожаловать в программу шифрования по методу 'шифр Цезаря'!");
             Console.WriteLine("Введите текст не длинее 200 символов с учетом пробелов и нажмите ENTER");
             string userInput = Console.ReadLine();
             Console.WriteLine("Вы написали:" + userInput);
             Console.WriteLine("Укажите величину сдвига в виде целого числа и нажмите ENTER");
             int shift = 0;
+            //int[] numbers = { 1, 2, 7, 21, -9};
+            //Console.WriteLine(numbers[3]);
             bool success = false;
             do
             {
@@ -28,6 +32,18 @@ namespace pk111CryptStruev
             //bool success = int.TryParse(Console.ReadLine(), out shift);
             Console.WriteLine("Вы указали:" + shift);
             Console.WriteLine("Успешность преобразования:" + success);
+            string newAlphabet = "";
+            for (int i = 0; i < 33; i++)
+            {
+                newAlphabet = newAlphabet + alphabet[i + shift];
+            }
+            Console.WriteLine(alphabet);
+            Console.WriteLine(newAlphabet);
+            for(int i = 0; i < userInput.Length; i++)
+            {
+                int number = alphabet.IndexOf(userInput[i]);
+                Console.Write(newAlphabet[number]);
+            }
             Console.ReadLine();
         }
     }
